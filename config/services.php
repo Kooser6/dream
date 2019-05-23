a31f9b2f6ff98662a5f52df54f904b6fb8d551a4
Nicholas <omatamix@gmail.com>
2019-05-23 12:49:33 -0500

    Update services.php

--- config/services.php
+++ config/services.php
@@ -47,18 +47,18 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => route('social.auth.callback', ['provider' => 'github']),
+        'redirect' => env('APP_URL') . '/login/github/callback',
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => route('social.auth.callback', ['provider' => 'google']),
+        'redirect' => env('APP_URL') . '/login/google/callback',
     ],
 
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => route('social.auth.callback', ['provider' => 'twitter']),
+        'redirect' => env('APP_URL') . '/login/twitter/callback',
     ],
 ];
