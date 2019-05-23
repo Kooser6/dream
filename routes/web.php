943f9058d26a922d00253aa58077dc04165e3d94
Nicholas <omatamix@gmail.com>
2019-05-23 12:20:30 -0500

    Add named route.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider')->where(['provider' => 'github|google|twitter']);
+Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider')->where(['provider' => 'github|google|twitter'])->name('social.auth');
 Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->where(['provider' => 'github|google|twitter']);
 
 Route::get('/home', 'HomeController@index')->name('home');
